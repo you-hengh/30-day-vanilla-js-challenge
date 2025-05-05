f850a42a2baaaabb6a375125b7a64c8ffe527a4c
冰冻大西瓜 <1003582810@qq.com>
2025-05-05 20:58:32 +0800

    :tada: 03-css-variables

--- packages/03-css-variables/src/main.js
+++ packages/03-css-variables/src/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+const inputs = document.querySelectorAll('.controls input')
+// 这里不要随意使用箭头函数,会导致this指向window
+function handleUpdate() {
+  const suffix = this.dataset.sizing || ''
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
